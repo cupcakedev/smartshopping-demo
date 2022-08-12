67168f0266c60b68a6566b3f19180a9248df1778
lenadweb <90589140+lenadweb@users.noreply.github.com>
2022-08-12 13:52:42 +0700

    update using storage

--- src/utils/sdkUtils.ts
+++ src/utils/sdkUtils.ts
@@ -1,50 +1,38 @@
-import { API_URL } from "../constants/constants";
-
-const tabsGet = (tabId: number): Promise<any> => {
-  const promise = new Promise((resolve, reject) => {
-    chrome.tabs.get(tabId, (tab) => {
-      const err = chrome.runtime.lastError;
-      if (err) {
-        reject(err);
-      } else {
-        resolve(tab);
-      }
-    });
-  });
-  return promise;
-};
+import { LocalStorageKeys, storage } from 'src/storage/config';
+import { tabsGet } from 'src/utils/tabUtils';
+import { API_URL } from '../constants/constants';
 
 export async function getApiUrl() {
-  const storageData = await localstoreGet(['env_isDevMod']);
-  const apiUrl = storageData?.env_isDevMod ? API_URL.dev : API_URL.prod;
-  return apiUrl;
+    const storageData = await storage.any.get([LocalStorageKeys.isDevMod]);
+    const apiUrl = storageData?.env_isDevMod ? API_URL.dev : API_URL.prod;
+    return apiUrl;
 }
 
 export async function requireShops() {
-  const apiUrl = await getApiUrl();
-  const response = await fetch(`${apiUrl}/demo/shops`);
-  const shops = await response.json();
-  await localstoreSet({ demoShops: shops });
+    const apiUrl = await getApiUrl();
+    const response = await fetch(`${apiUrl}/demo/shops`);
+    const shops = await response.json();
+    await storage.any.set({ demoShops: shops });
 }
 
 export async function requirePromocodes(id: number): Promise<Array<string>> {
-  const activeTab = await tabsGet(id);
-  if (!activeTab) return [];
-  const url = activeTab.pendingUrl || activeTab.url || '';
+    const activeTab = await tabsGet(id);
+    if (!activeTab) return [];
+    const url = activeTab.pendingUrl || activeTab.url || '';
 
-  const storageData = await localstoreGet(['demoShops']);
-  if (!storageData.demoShops) return [];
-  const apiUrl = await getApiUrl();
+    const demoShops = await storage.any.get(LocalStorageKeys.demoShops);
+    if (!demoShops) return [];
+    const apiUrl = await getApiUrl();
 
-  const locatedShop = storageData.demoShops.find(
-    (shop: { id: string; urlPattern: string }) => {
-      const pattern = new RegExp(shop.urlPattern);
-      return pattern.test(url);
-    }
-  );
-  if (!locatedShop) return [];
+    const locatedShop = demoShops.find(
+        (shop: { id: string; urlPattern: string }) => {
+            const pattern = new RegExp(shop.urlPattern);
+            return pattern.test(url);
+        }
+    );
+    if (!locatedShop) return [];
 
-  const response = await fetch(`${apiUrl}/demo/${locatedShop.id}`);
-  const promocodes = await response.json();
-  return promocodes;
+    const response = await fetch(`${apiUrl}/demo/${locatedShop.id}`);
+    const promocodes = await response.json();
+    return promocodes;
 }
