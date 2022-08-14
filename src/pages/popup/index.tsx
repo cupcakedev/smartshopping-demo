1c3e3ee09865667b959ee0c8dfb3a33c10557d13
lenadweb <90589140+lenadweb@users.noreply.github.com>
2022-08-14 19:39:03 +0700

    update popup entrypoint
    implementation with react 18

--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Popup } from './components/Popup';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const rootEl = document.getElementById('root');
-    ReactDOM.render(<Popup />, rootEl);
-});
+const root = document.createElement('div');
+root.id = 'root';
+document.body.appendChild(root);
+
+ReactDOM.createRoot(root).render(<Popup />);
