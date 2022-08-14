163c9f1eb32c63a485841894bc9d9657b125d9cd
lenadweb <90589140+lenadweb@users.noreply.github.com>
2022-08-14 15:21:13 +0700

    Fix extra brace

--- src/pages/popup/components/styled_components.ts
+++ src/pages/popup/components/styled_components.ts
@@ -10,19 +10,18 @@ const Container = styled.div`
 `;
 
 const Button = styled.button<{ enabled: boolean }>`
-  width: 160px;
-  height: 40px;
-  border-radius: 10px;
-  font-size: 20px;
-  text-align: center;
-  outline: none;
-  border: 3px solid
-    ${COLORS.primary};
-  background-color: ${(props) =>
-      props.enabled ? COLORS.primary : COLORS.white}};
-  color: ${(props) => (props.enabled ? COLORS.white : COLORS.primary)};
-  transition: background-color 0.2s;
-  cursor: pointer;
+    width: 160px;
+    height: 40px;
+    border-radius: 10px;
+    font-size: 20px;
+    text-align: center;
+    outline: none;
+    border: 3px solid ${COLORS.primary};
+    background-color: ${(props) =>
+        props.enabled ? COLORS.primary : COLORS.white};
+    color: ${(props) => (props.enabled ? COLORS.white : COLORS.primary)};
+    transition: background-color 0.2s;
+    cursor: pointer;
 `;
 
 export { Container, Button };
