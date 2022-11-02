618749c4f1725b8a1c7608af12b1fd0a04b42e6d
lenadweb <90589140+lenadweb@users.noreply.github.com>
2022-11-02 19:09:34 +0700

    add new Colors

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,11 +1,14 @@
 export const COLORS = {
     primary: '#6B5DEF',
+    primaryDark: '#5b4ed1',
     secondary: '#6AAEF4',
     dark: '#2B2D33',
+    darkGrey: '#171717',
     grey: '#8F9096',
     lightGrey: '#BABEC6',
     extraLightGrey: '#EFF0F2',
     white: '#FFF',
+    black: '#000000',
     green: 'green',
     red: 'red',
 };
